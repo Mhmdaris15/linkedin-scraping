502f3c49df95d3bb144bda7c548f6c124ae728ca
Muhammad Aris Septanugroho <78630435+Mhmdaris15@users.noreply.github.com>
2023-11-09 17:20:19 +0700

    Create README.md

---
+++ learn-selenium/main.py
@@ -0,0 +1,61 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class LinkedInBot:
+    def __init__(self, username, password):
+        # self.chrome_driver_path = self._get_chrome_driver_path()
+        self.driver = webdriver.Chrome()
+        self.username = username
+        self.password = password
+
+    def _get_chrome_driver_path(self):
+        os.environ["PATH"] += os.pathsep + os.getcwd() + r"\ChromeDriver"
+        return os.environ["PATH"].split(";")[-1]
+
+    def login(self):
+        self.driver.get("https://www.linkedin.com/login")
+        self.driver.find_element(By.ID, "username").send_keys(self.username)
+        self.driver.find_element(By.ID, "password").send_keys(self.password)
+        login_button = self.driver.find_element(
+            By.CSS_SELECTOR, '[data-litms-control-urn="login-submit"]'
+        )
+        login_button.click()
+        self.driver.implicitly_wait(10)
+
+    def visit_profile(self, profile_url):
+        self.driver.get(profile_url)
+
+    def search_job(self, job_name):
+        self.driver.get(
+            "https://www.linkedin.com/jobs/search/?keywords={}&location=Indonesia".format(
+                job_name
+            )
+        )
+
+    def get_html(self):
+        return self.driver.page_source
+
+    def close(self):
+        self.driver.quit()
+
+
+# Example usage
+if __name__ == "__main__":
+    username = "rafinabil39@gmail.com"
+    password = "Z$jptLTvw4jd-&J"
+    os.environ["PATH"] += os.pathsep + os.getcwd() + r"\ChromeDriver"
+    print(os.environ["PATH"].split(";")[-1])
+    chrome_driver_path = os.environ["PATH"].split(";")[-1]
+
+    linkedin_bot = LinkedInBot(username, password)
+
+    try:
+        linkedin_bot.login()
+        # profile_url = "https://www.linkedin.com/in/"
+        # linkedin_bot.visit_profile(profile_url)
+        linkedin_bot.search_job("Software Engineer")
+    finally:
+        # linkedin_bot.close()
+        input("End of the program!")
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
-import math
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-target_url = "https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/3698543802"
-number_of_loops = math.ceil(400/25)
-print("Number of loops: ", number_of_loops)
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# Navigate to the LinkedIn login page
+driver.get("https://www.linkedin.com/login")
+
+# Enter your email address and password
+driver.find_element_by_id("username").send_keys("rafinabil39@gmail.com")
+driver.find_element_by_id("password").send_keys("Z$jptLTvw4jd-&J")
+# Submit the login form
+driver.find_element_by_css_selector(".login__form_action_container button").click()
+
+profile_url = "https://www.linkedin.com/in/example-profile"
+driver.get(profile_url)
